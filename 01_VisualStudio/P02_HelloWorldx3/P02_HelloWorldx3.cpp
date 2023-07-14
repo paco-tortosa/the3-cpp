79b943b5cf637d2c26770d2c71337a86c7995d56
frjatoma <paco.tortosa.martinez@gmail.com>
2023-07-14 11:08:12 +0200

    P02 created

---
+++ 01_VisualStudio/P02_HelloWorldx3/P02_HelloWorldx3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+void PACO_HelloWorld();
+void JAVI_HelloWorld();
+void JUAN_HelloWorld();
+
+int main()
+{
+    PACO_HelloWorld();
+    JAVI_HelloWorld();
+    JUAN_HelloWorld();
+}
---
+++ 01_VisualStudio/P02_HelloWorldx3/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+void PACO_HelloWorld() {
+	std::cout << "Paco: Hello World" << std::endl;
+}
+
+void JAVI_HelloWorld() {
+}
+
+void JUAN_HelloWorld() {
+}
